607172e7a81d556b312beb35aa5874db52848852
venusgalstar <venusgalstar@gmail.com>
2022-09-12 18:33:51 +0000

    testing

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var config = {
     // original
     primaryKey: "644f579152f99037f56150bcac0420b69936dbf5b29b3267cbe9ce21601c3060",
 
-    // saving
+    // saving account
     publicKey: "0x4C3Ee952f0A76E21C14D491B2C0313605D1781E4",
     
 };
