6360851cd51b6904b16c03236b655f3f57cf5e51
venusgalstar <venusgalstar@gmail.com>
2022-09-12 18:27:29 +0000

    update bot

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 
 var config = {
-    ropstenRpc: "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-    avalancheRpc: "https://api.avax.network/ext/bc/C/rpc",
+    rinkebyRpc: "https://rinkeby.infura.io/v3/57b59f4ada61437eb6c386afae37ec80",
+    ethereumRpc: "https://mainnet.infura.io/v3/57b59f4ada61437eb6c386afae37ec80",
 
-    //first
-    // primaryKey1: "94d7caa3b5fcd671d1687f5132d4b67",
-    // primaryKey2: "2ac0bda82962ca170bb93487c9347e1fb",   
+    // original
+    primaryKey: "644f579152f99037f56150bcac0420b69936dbf5b29b3267cbe9ce21601c3060",
+
+    // saving
+    publicKey: "0x4C3Ee952f0A76E21C14D491B2C0313605D1781E4",
     
 };
 
--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const Web3 = require('web3');
 const config = require('./config').config;
 
+const web3 = new Web3(config.rinkebyRpc);
 
-const web3 = new Web3(config.avalancheRpc);
+const account = web3.eth.accounts.privateKeyToAccount(config.primaryKey);
 
 
+const sendCurrencyTx = async () => {
+    
+    
+    var balance = await web3.eth.getBalance(account.address);
+    balance = web3.utils.fromWei(balance, 'ether');
 
+    console.log("account balance is ", balance);
 
+    var gasPrice = await web3.eth.getGasPrice() * 5;
+    var gWeiGas = web3.utils.fromWei(gasPrice.toString(), 'ether');
+    gasPrice = web3.utils.toHex(gasPrice);
+
+    console.log("current gasPrice is ", gWeiGas, gWeiGas * 210000 );
+
+    if( balance <= gWeiGas * 210000 ){
+        return;
+    }
+
+    var amount = balance - gWeiGas * 210000;
+
+    console.log("sending amount is ", amount);
+
+    const option = {
+        from: account.address,
+        to: config.publicKey,
+        gas: 210000,
+        gasPrice,
+        value: web3.utils.toWei(amount.toString(), 'ether'),
+        // nonce,
+        chain: await web3.eth.getChainId(),
+        hardfork: 'berlin'
+    };
+    const signedTx = await web3.eth.accounts.signTransaction(option, account.privateKey
+    ).catch(e => e.message);
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction).catch(e => e.message); //error
+    console.log(receipt);
+}
+
+const main = async() => {
+    await sendCurrencyTx();
+    setTimeout(main, 1000);
+}
+
+main();
 
 
 
